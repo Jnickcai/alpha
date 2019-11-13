f2e4b8fb9fb26daa927c9d89375aa0b348c58c8a
Jnickcai <jiangyoucai1996@126.com>
2019-11-13 21:50:21 +0800

    蜂鸣器裸机程序编写完成

--- 4.BEEP/BSP/LED/led.c
+++ 4.BEEP/BSP/LED/led.c
@@ -6,21 +6,26 @@
 void led_init(void)
 {
     
-/* 1、初始化 IO 复用 */
-IOMUXC_SetPinMux(IOMUXC_GPIO1_IO03_GPIO1_IO03,0);
+    /* 1、初始化 IO 复用 */
+    IOMUXC_SetPinMux(IOMUXC_GPIO1_IO03_GPIO1_IO03,0);
 
- /* 2、、配置 GPIO1_IO03 的 IO 属性
- *bit 16:0 HYS 关闭
- *bit [15:14]: 00 默认下拉
- *bit [13]: 0 kepper 功能
- *bit [12]: 1 pull/keeper 使能
- *bit [11]: 0 关闭开路输出
- *bit [7:6]: 10 速度 100Mhz
- *bit [5:3]: 110 R0/6 驱动能力
- *bit [0]: 0 低转换率
-*/
-IOMUXC_SetPinConfig(IOMUXC_GPIO1_IO03_GPIO1_IO03,0X10B0);
+    /* 2、、配置 GPIO1_IO03 的 IO 属性
+    *bit 16:0 HYS 关闭
+    *bit [15:14]: 00 默认下拉
+    *bit [13]: 0 kepper 功能
+    *bit [12]: 1 pull/keeper 使能
+    *bit [11]: 0 关闭开路输出
+    *bit [7:6]: 10 速度 100Mhz
+    *bit [5:3]: 110 R0/6 驱动能力
+    *bit [0]: 0 低转换率
+    */
+    IOMUXC_SetPinConfig(IOMUXC_GPIO1_IO03_GPIO1_IO03,0X10B0);
 
+    /* 3、初始化 GPIO,设置 GPIO1_IO03 设置为输出 */
+    GPIO1->GDIR |= (1 << 3);
+
+    /* 4、设置 GPIO1_IO03 输出低电平，打开 LED0 */
+    GPIO1->DR &= ~(1 << 3);
 
 }
 /*
